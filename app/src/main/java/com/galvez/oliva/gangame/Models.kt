fef4742f62a89a77d95885fc5235064f5f66b750
OLIVA GALVEZ GARCIA <Oliva.Galvez@alu.uclm.es>
2018-06-18 14:43:22 +0200

    Asignación Automática

--- app/src/main/java/com/galvez/oliva/gangame/Models.kt
+++ app/src/main/java/com/galvez/oliva/gangame/Models.kt
@@ -1,8 +1,16 @@
 package com.galvez.oliva.gangame
 
+import java.text.Format
+
 /**
  * Created by Oliva on 18/06/2018.
  */
+
+object PriceFormatted {
+    val FORMAT_PRICE = "$%.2f"
+
+    fun priceFormatted (price: Float) = String.format(FORMAT_PRICE, price)
+}
 
 /*Definir los dos modelos de datos*/
 
@@ -11,7 +19,14 @@ data class Deal (var title:String,
                  var normalPrice:Float,
                  var metacriticScore:Int,
                  var steamRating: Int,
-                 var thumb:String)
+                 var thumb:String){
+
+    val salePriceFormatted: String
+        get() = PriceFormatted.priceFormatted(salePrice)
+
+    val normalPriceFormatted: String
+        get() = PriceFormatted.priceFormatted(normalPrice)
+}
 
 data class TopGame (var title: String,
                     var owners:String,
@@ -19,4 +34,8 @@ data class TopGame (var title: String,
                     var publisher: String,
                     var price: Float,
                     var position: Int,
-                    var thumb: String)
+                    var thumb: String){
+
+    val priceFormatted: String
+        get() = PriceFormatted.priceFormatted(price)
+}
