107591d762463046120257c31a81bf9f2aabf87d
OLIVA GALVEZ GARCIA <Oliva.Galvez@alu.uclm.es>
2018-06-21 13:52:44 +0200

    Creación del builder y adición de convertidor

--- gangame.sdk/src/main/java/com/galvez/oliva/gangamesdk/GangameApiService.kt
+++ gangame.sdk/src/main/java/com/galvez/oliva/gangamesdk/GangameApiService.kt
@@ -1,7 +1,24 @@
 package com.galvez.oliva.gangamesdk
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 /**
  * Created by Oliva on 21/06/2018.
  */
 class GangameApiService {
+
+    val apiClient: RetrofitGangameApi
+
+    //Configuración del cliente
+    init {
+        val apiClientConfig =
+                Retrofit.Builder()
+                        .baseUrl(Routes.BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+
+        //El create nos guarda la instancia de este cliente
+        apiClient = apiClientConfig.create(RetrofitGangameApi::class.java)
+    }
 }
