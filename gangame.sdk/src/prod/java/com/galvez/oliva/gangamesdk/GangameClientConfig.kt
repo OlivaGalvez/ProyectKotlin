d72d9ef3b04c564b31d9e0918a79d6101550e1ec
OLIVA GALVEZ GARCIA <Oliva.Galvez@alu.uclm.es>
2018-06-22 13:34:37 +0200

    Configuración del cliente por flavor

---
+++ gangame.sdk/src/prod/java/com/galvez/oliva/gangamesdk/GangameClientConfig.kt
@@ -0,0 +1,12 @@
+package com.galvez.oliva.gangamesdk
+
+import retrofit2.Retrofit
+
+/**
+ * Created by Oliva on 22/06/2018.
+ */
+class GangameClientConfig: GangameApiConfig {
+    override fun setupConfig(builder: Retrofit.Builder) {
+
+    }
+}
